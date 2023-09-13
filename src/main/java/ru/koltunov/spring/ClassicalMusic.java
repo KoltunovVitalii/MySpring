3fb133d7b11d2681486a6c23fee5a87f5cb86ff3
Vitalii Koltunov <Koltunov.vitalii@gmail.com>
2023-09-13 10:44:37 +0500

    MySpring

---
+++ src/main/java/ru/koltunov/spring/ClassicalMusic.java
@@ -0,0 +1,10 @@
+package ru.koltunov.spring;
+
+public class ClassicalMusic implements Music{
+    @Override
+    public String getSong() {
+
+        return "Hungarian Rhapsody";
+
+    }
+}
---
+++ src/main/java/ru/koltunov/spring/RockMusic.java
@@ -0,0 +1,10 @@
+package ru.koltunov.spring;
+
+public class RockMusic implements Music{
+    @Override
+    public String getSong() {
+
+        return "Wind cries Mary";
+
+    }
+}
