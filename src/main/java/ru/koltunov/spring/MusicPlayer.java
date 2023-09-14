14de8183033b2d6540997febc9ddcfedb8287619
Vitalii Koltunov <Koltunov.vitalii@gmail.com>
2023-09-14 11:14:59 +0500

    MySpring2

--- src/main/java/ru/koltunov/spring/MusicPlayer.java
+++ src/main/java/ru/koltunov/spring/MusicPlayer.java
@@ -5,6 +5,7 @@ public class MusicPlayer {
     public MusicPlayer(Music music) {
         this.music = music;
     }
+
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
     }
